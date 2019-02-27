2d205a178b54f7f491eef071293962f45778bcca
Gianpc23 <gianfrancoinformaticaperez@gmail.com>
2019-02-27 12:25:52 +0100

    Terminado el punto 3

--- los_almacenes.sql
+++ los_almacenes.sql
@@ -37,18 +37,33 @@ INNER JOIN almacenes a ON a.Codigo = c.Almacen GROUP BY a.Codigo;
 
 /*3.9. Obtener los códigos de los almacenes que estén saturados (los almacenes
 donde el número de cajas es superior a la capacidad).*/
-SELECT a.Codigo FROM almacenes a
-INNER JOIN cajas c ON a.Codigo = c.Almacen;
+-- FALTAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
+/*3.10. Obtener los números de referencia de las cajas que están en Bilbao.*/
+SELECT c.NumReferencia FROM cajas c
+INNER JOIN almacenes a ON a.Codigo = c.Almacen WHERE a.Lugar = "Bilbao";
 
-SELECT a.Codigo, a.Capacidad FROM almacenes a INNER JOIN cajas c ON a.Codigo = c.Almacen GROUP BY a.Codigo; 
+/*3.11. Insertar un nuevo almacén en Barcelona con capacidad para 3 cajas.*/
+INSERT INTO almacenes(Lugar,Capacidad) VALUES("Barcelona",3);
 
-SELECT COUNT(*) FROM cajas c
-INNER JOIN almacenes a ON a.Codigo = c.Almacen GROUP BY a.Codigo
+/*3.12. Insertar una nueva caja, con número de referencia 'H5RT', con contenido
+'Papel', valor 200, y situada en el almacén 2.*/
+INSERT INTO cajas(NumReferencia,Contenido,Valor,Almacen) VALUES ("H5RT","Papel",200,2);
 
+/*3.13. Rebajar el valor de todas las cajas un 15 %.*/
+UPDATE cajas SET Valor = Valor * 0.85;
 
+/*3.14. Rebajar un 20 % el valor de todas las cajas cuyo valor sea superior al valor
+medio de todas las cajas.*/
+UPDATE cajas SET Valor = Valor * 0.80 WHERE Valor >(
+	SELECT AVG(Valor) as Valor
+); -- NO FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
+/*3.15. Eliminar todas las cajas cuyo valor sea inferior a 100 €.*/
+DELETE FROM cajas WHERE Valor < 100;
 
+/*3.16. Vaciar el contenido de los almacenes que estén saturados.*/
+-- FALTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 
 
