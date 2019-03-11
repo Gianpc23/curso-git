9dba6b223ad3f7d9635b9e2afc7550aa33aad9b6
Gianpc23 <gianfrancoinformaticaperez@gmail.com>
2019-03-11 11:04:23 +0100

    Componentes de ejemplo añadidos

--- angular/proyecto/src/app/app.module.ts
+++ angular/proyecto/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+import { OtroComponenteComponent } from './otro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiPrimerComponenteComponent,
+    OtroComponenteComponent
   ],
   imports: [
     BrowserModule
